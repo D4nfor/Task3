24e182636220a4bcb95ae97176b62ec97bc4f001
D4nfor <daniilmatuhin@mail.ru>
2025-12-27 22:54:07 +0300

    Добавлен класс Vertex с координатами и вычислением расстояния

---
+++ src/geometry/Vertex.java
@@ -0,0 +1,77 @@
+package geometry;
+
+/**
+ * Класс для представления вершины в 3D пространстве
+ */
+public class Vertex {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    /**
+     * Вычислить расстояние до другой вершины
+     */
+    public double distanceTo(Vertex other) {
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        double dz = this.z - other.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%.2f, %.2f, %.2f)", x, y, z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Vertex vertex = (Vertex) obj;
+        return Double.compare(vertex.x, x) == 0 &&
+                Double.compare(vertex.y, y) == 0 &&
+                Double.compare(vertex.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        long temp = Double.doubleToLongBits(x);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(z);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
